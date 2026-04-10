c697ee4c8a314933262c3e778040bdc702444222
samidup <dupuy.sami@gmail.com>
2026-04-10 15:29:57 +0200

    fix: real PWA manifest + SW for APK

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'iron-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return res;
+    })).catch(() => caches.match('./index.html'))
+  );
+});
